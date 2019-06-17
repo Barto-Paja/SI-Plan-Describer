1228f69f385e99e5e69831a952e72be6c5832090
Bartosz Pajewski <bartosz.pajewski@gmail.com>
2019-06-17 22:01:23 +0200

    Init test project

---
+++ test/tst_testdocuments.cpp
@@ -0,0 +1,35 @@
+#include <QtTest>
+
+// add necessary includes here
+
+class testDocuments : public QObject
+{
+    Q_OBJECT
+
+public:
+    testDocuments();
+    ~testDocuments();
+
+private slots:
+    void test_case1();
+
+};
+
+testDocuments::testDocuments()
+{
+
+}
+
+testDocuments::~testDocuments()
+{
+
+}
+
+void testDocuments::test_case1()
+{
+
+}
+
+QTEST_APPLESS_MAIN(testDocuments)
+
+#include "tst_testdocuments.moc"
